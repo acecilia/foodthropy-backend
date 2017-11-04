5aecccc8990d5a2d60072cf62bd7617e530d880b
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-11-04 14:32:30 +0800

    Better dummy

--- Sources/App/Controllers/LocationController.swift
+++ Sources/App/Controllers/LocationController.swift
@@ -1,22 +1,15 @@
 import Foundation
 
-class LocationController: DefaultController<Location> { }
+final class LocationController: DefaultController<Location> { }
 
 extension LocationController: DummyFillable {
     static func dummyFill() throws {
-        if try Location.count() == 0 {
-            let nf = NumberFormatter()
-            nf.numberStyle = .spellOut
-            nf.locale = Locale(identifier: "en_US")
-            
-            // Reset id count
-            try Location.database?.raw("ALTER SEQUENCE locations_id_seq RESTART WITH 1")
-            
-            for number in 1...200 {
-                if let name = nf.string(from: NSNumber(value: number)) {
-                    try Location(name: name).save()
-                }
-            }
+        try Location.makeQuery().delete()
+        // Reset id count
+        try Location.database?.raw("ALTER SEQUENCE locations_id_seq RESTART WITH 1")
+        
+        for number in 1...200 {
+            try Location(name: "Food Court \(number)").save()
         }
     }
 }
