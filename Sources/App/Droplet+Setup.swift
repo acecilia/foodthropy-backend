da2ec100a35f688abfc50a0eaa39d9c74f07bc17
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-11-05 22:20:46 +0800

    Exposed drop

--- Sources/App/Droplet+Setup.swift
+++ Sources/App/Droplet+Setup.swift
@@ -1,7 +1,10 @@
 @_exported import Vapor
 
+private(set) var drop: Droplet!
+
 extension Droplet {
     public func setup() throws {
+        drop = self
         try setupRoutes()
         // Do any additional droplet setup
     }
